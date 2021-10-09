6c5cbc1ce8e3b1347434c477e5472df639453d06
alifianputrasugiharto <90169192+alifianputrasugiharto@users.noreply.github.com>
2021-10-09 19:49:04 +0700

    problem_8
    problem_8

---
+++ Assets/script/BoxCoinProb8.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCoinProb8 : MonoBehaviour
+{
+    public Vector2 scaleRandomValue;
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    public void Spawn()
+    {
+        bool boxSpawned = false;
+        while (!boxSpawned)
+        {
+            Vector2 spawnPosition = GameManagerProb8.Instance.GetRandomPosition();
+            if(((Vector2)player.position - spawnPosition).magnitude < 2)
+            {
+                continue;
+            }
+            else
+            {
+                transform.position = spawnPosition;
+                Setup();
+                boxSpawned = true;
+            }
+        }
+    }
+
+    private void Setup()
+    {
+        float xScale = Random.Range(scaleRandomValue.x, scaleRandomValue.y);
+        float yScale = Random.Range(scaleRandomValue.x, scaleRandomValue.y);
+
+        transform.localScale = new Vector2(xScale, yScale);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManagerProb8.Instance.AddScore();
+            GameManagerProb8.Instance.RespawnBox();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/script/GameManagerProb8.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerProb8 : MonoBehaviour
+{
+    #region SINGLETON
+    private static GameManagerProb8 _instance;
+    public static GameManagerProb8 Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManagerProb8>();
+                if (_instance == null) Debug.LogError("No Game Manager Found!!!");
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public int Score { get; private set; }
+
+    [Header("Box Coin Controller")]
+    public int coinSpawn;
+    [SerializeField] BoxCoinProb8 boxCoinPrefab;
+    private List<BoxCoinProb8> boxCoinsPool = new List<BoxCoinProb8>();
+
+    [Header("Game area constraint")]
+    public float areaConstraintValue = 5f;
+
+    [Header("UI")]
+    public Text scoreText;
+
+    private void Start()
+    {
+        for (int i = 0; i < coinSpawn; i++)
+        {
+            BoxCoinProb8 coin = GetBox();
+            coin.Spawn();
+        }
+        scoreText.text = $"Score\n{Score}";
+    }
+
+    public Vector2 GetRandomPosition()
+    {
+        float xPosition = Random.Range(-areaConstraintValue, areaConstraintValue);
+        float yPosition = Random.Range(-areaConstraintValue, areaConstraintValue);
+
+        return new Vector2(xPosition, yPosition);
+    }
+
+    public void AddScore()
+    {
+        Score++;
+        scoreText.text = $"Score\n{Score}";
+    }
+
+
+    public void RespawnBox() => StartCoroutine(ReSpawnBox());
+    IEnumerator ReSpawnBox()
+    {
+        yield return new WaitForSeconds(3);
+        BoxCoinProb8 coin = GetBox();
+        coin.Spawn();
+    }
+
+    public BoxCoinProb8 GetBox()
+    {
+        for (int i = 0; i < boxCoinsPool.Count; i++)
+        {
+            if (!boxCoinsPool[i].gameObject.activeSelf)
+            {
+                boxCoinsPool[i].gameObject.SetActive(true);
+                return boxCoinsPool[i];
+            }
+        }
+
+        BoxCoinProb8 boxObject = Instantiate(boxCoinPrefab, transform);
+        boxCoinsPool.Add(boxObject);
+        return boxObject;
+    }
+}
